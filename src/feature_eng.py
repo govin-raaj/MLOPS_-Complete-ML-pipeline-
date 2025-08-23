43cea3e4fc4cce1f56f4cdb0184bf63c3ad0dbe4
govin-raaj <govin30raaj@gmail.com>
2025-08-23 12:55:24 +0530

    params added

--- src/feature_eng.py
+++ src/feature_eng.py
@@ -24,6 +24,25 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
+
+
 def load_data(data_path: str) -> pd.DataFrame:
     """Load data from a CSV file."""
     try:
@@ -74,7 +93,9 @@ def save_data(df: pd.DataFrame,  data_path: str) -> None:
 
 def main():
     try:
-        max_features = 50
+        #max_features = 50
+        params = load_params(params_path='params.yaml')
+        max_features = params['feature_engineering']['max_features']
         
         # Split the data into train and test sets
         train_data = load_data('./data/interim/train_processed.csv')
